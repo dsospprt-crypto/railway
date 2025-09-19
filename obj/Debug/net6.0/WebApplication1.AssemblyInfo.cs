40952b0975ebb2dfee22bd97264da49ca2965d38
KrIsH9693 <aimaker981@gmail.com>
2025-09-19 16:54:41 +0530

    Add Dockerfile for Render

--- obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9266db0bdf91a48fcf56bd5b5cc65496e7bae25")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
